44402fdcffb977ffedfc1e1266e952cf6d99c84f
akiman410 <booshkimani@comcast.net>
2022-03-22 16:02:01 -0400

    Second update

--- movieshopspa/src/app/core/services/movie.service.ts
+++ movieshopspa/src/app/core/services/movie.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MovieCard } from 'src/app/shared/models/moviecard'
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getTopGrossingMovies(): Observable<MovieCard[]>{
+   return this.http.get<MovieCard[]>(`${environment.apiBaseUrl}Movies/top-grossing`)
+    //call the API to get array of movies
+    //Make GET HTTP call
+    //XMLHTTP Request
+    // Angular uses HTTPClient Class to make XMLHTTP behind the scenes
+    // Uses HttpClient we have to import HttpClient module
+    //Observable as better version of promises
+    //RxJS
+  }
 }
--- movieshopspa/src/app/home/home.component.ts
+++ movieshopspa/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../core/services/movie.service';
+import { MovieCard } from '../shared/models/moviecard';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  movieCards!:MovieCard[];
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    console.log('inside ngOn Init life cycle')
+    this.movieService.getTopGrossingMovies().subscribe
+    ( m=>{
+      this.movieCards=m;
+      console.log(this.movieCards)
+    }
+    );
   }
   //get to 30 movies from API and send the data to the View
   //call the API https://localhost:7051/api/Movies/top-grossing 
