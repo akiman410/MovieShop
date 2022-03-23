4e1d974f3f7f060225f798107fa95eff2a4657a9
akiman410 <booshkimani@comcast.net>
2022-03-23 09:57:18 -0400

    userAdminMovieComponents

--- movieshopspa/src/app/app-routing.module.ts
+++ movieshopspa/src/app/app-routing.module.ts
@@ -6,9 +6,18 @@ const routes: Routes = [
   {
     path:"",component: HomeComponent
   },
-  //{
-    //path:'movies/details/:id', component: MovieDetailsComponent
-  //}
+  {
+    path: "admin", loadChildren: () => import("./admin/admin.module").then(mod => mod.AdminModule)
+  },
+  {
+    path: "movies", loadChildren: () => import("./movies/movies.module").then(mod => mod.MoviesModule)
+  },
+  {
+    path: "user", loadChildren: () => import("./user/user.module").then(mod => mod.UserModule)
+  },
+  {
+    path: "account", loadChildren: () => import("./account/account.module").then(mod => mod.AccountModule)
+  }
 ];
 
 @NgModule({
