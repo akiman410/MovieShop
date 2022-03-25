47d82c502c4fb475a9765aab7587ea7844490ac2
akiman410 <booshkimani@comcast.net>
2022-03-25 11:12:36 -0400

    Logout Login and Register Button

--- movieshopspa/src/app/core/layout/header/header.component.ts
+++ movieshopspa/src/app/core/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/User';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean =false;
+  user!: User;
 
-  constructor() { }
+
+  constructor(private authService: AuthenticationService, private router: Router) { 
+    this.authService.isLoggedIn.subscribe( resp => this.isLoggedIn=resp);
+    this.authService.currentUser.subscribe( resp => this.user=resp);
+  }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    console.log('logout');
+    this.authService.logout();
+    this.router.navigateByUrl('/account/login');
+  }
 }
