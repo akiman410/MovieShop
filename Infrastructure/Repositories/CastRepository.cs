ffd98a12f9cb58fc27116d129cf45fedc391e11e
akiman410 <booshkimani@comcast.net>
2022-03-28 06:12:05 -0400

    Cast

--- Infrastructure/Repositories/CastRepository.cs
+++ Infrastructure/Repositories/CastRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ApplicationCore.Contracts.Repositories;
 using ApplicationCore.Enities;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,12 @@ namespace Infrastructure.Repositories
     {
         public CastRepository(MovieShopDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public  async Task<Cast> GetCastDetailsById(int id)
+        {
+            var castDetails = await _dbContext.Casts.Include(m => m.MovieCasts).ThenInclude(m => m.Movie).FirstOrDefaultAsync(m => m.Id == id);
+            return castDetails; ;
         }
     }
 }
